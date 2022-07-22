eaa6a94d466f1f6ce095340476ff6ae6553218f5
Molly <molly.gouletas@gmail.com>
2022-07-22 14:52:41 -0700

    Add interfaces for handling connection between processes and persistence layer

---
+++ adapters/handler_interfaces.go
@@ -0,0 +1,17 @@
+package adapters
+
+import (
+	"github.com/freemish/simple-bank-ledger/entities"
+)
+
+type IAuthProcessHandler interface {
+	Login(username, password string) (*entities.Customer, error)
+	Register(name, username, password string) (*entities.Customer, error)
+	Logout(username string) error
+}
+
+type ITransactionProcessHandler interface {
+	GetBalance(cust *entities.Customer) (float64, error)
+	GetTransactions(cust *entities.Customer, args ...map[string]interface{})
+	RecordTransaction(cust *entities.Customer, is_debit bool, amount float64, voided_tx int) (*entities.Transaction, error)
+}
