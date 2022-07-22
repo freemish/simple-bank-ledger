1af67e99e2f94c4b6af8e718f895711514c1c8b7
Molly <molly.gouletas@gmail.com>
2022-07-22 16:32:19 -0700

    Add more tests

--- persistence/in_memory_cache_test.go
+++ persistence/in_memory_cache_test.go
@@ -1,7 +1,6 @@
 package persistence
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/freemish/simple-bank-ledger/entities"
@@ -13,34 +12,6 @@ func TestSelectCustomerByUsernameDoesNotExist(t *testing.T) {
 	_, err := imc.SelectCustomerByUsername("doesnotexist")
 	if err != processes.ErrCustomerDoesNotExist {
 		t.Error(err)
-	}
-}
-
-func TestInsertCustomerAlreadyExists(t *testing.T) {
-	var imc = InMemoryCacheStore{customers: make(map[string]*entities.Customer)}
-	imc.customers["goopy"] = &entities.Customer{Username: "goopy"}
-	err := imc.InsertCustomer(imc.customers["goopy"])
-	if err != processes.ErrCustomerAlreadyExists {
-		t.Errorf("Expected customer already exists error and got: %s", err)
-	}
-}
-
-func TestCreateAccount(t *testing.T) {
-	var imc = InMemoryCacheStore{customers: make(map[string]*entities.Customer)}
-	imc.customers["goopy"] = &entities.Customer{Username: "Gooper"}
-	fmt.Println(imc.customers)
-	cust, err := processes.CreateAccount("moo", "Molly", "1234", imc)
-	if err != nil {
-		t.Errorf("Unexpected error while creating account: %v", err.Error())
-		return
-	}
-	if cust == nil {
-		t.Error("Customer should not be nil")
-		return
-	}
-	if cust.Username != "moo" {
-		t.Errorf("Unexpected customer username: %v", cust.Username)
-		return
 	}
 }
 
@@ -63,5 +34,53 @@ func TestSelectCustomerByUsername(t *testing.T) {
 	}
 	if cust.Username != "molly" {
 		t.Errorf("Unknown error while selecting newly created customer: %v", cust)
+	}
+}
+
+func TestInsertCustomerAlreadyExists(t *testing.T) {
+	var imc = InMemoryCacheStore{customers: make(map[string]*entities.Customer)}
+	imc.customers["goopy"] = &entities.Customer{Username: "goopy"}
+	err := imc.InsertCustomer(imc.customers["goopy"])
+	if err != processes.ErrCustomerAlreadyExists {
+		t.Errorf("Expected customer already exists error and got: %s", err)
+	}
+}
+
+func TestInsertCustomer(t *testing.T) {
+	var imc = InMemoryCacheStore{customers: make(map[string]*entities.Customer)}
+	err := imc.InsertCustomer(&entities.Customer{Username: "goopy"})
+	if err != nil {
+		t.Errorf("Expected nil error while inserting customer and got: %s", err)
+	}
+}
+
+func TestCreateAccount(t *testing.T) {
+	var imc = InMemoryCacheStore{customers: make(map[string]*entities.Customer)}
+	cust, err := processes.CreateAccount("moo", "Molly", "1234", imc)
+	if err != nil {
+		t.Errorf("Unexpected error while creating account: %v", err.Error())
+		return
+	}
+	if cust == nil {
+		t.Error("Customer should not be nil")
+		return
+	}
+	if cust.Username != "moo" {
+		t.Errorf("Unexpected customer username: %v", cust.Username)
+		return
+	}
+}
+
+func TestLogin(t *testing.T) {
+	var imc = InMemoryCacheStore{customers: make(map[string]*entities.Customer)}
+	processes.CreateAccount("moo", "Molly", "1234", imc)
+	cust, err := processes.Login("moo", "1234", imc)
+	if err != nil {
+		t.Errorf("Login should have passed, but errored out: %v", err)
+	}
+	if cust == nil {
+		t.Error("Customer was nil from Login")
+	} else if cust.Username != "moo" {
+		t.Errorf("Logged in wrong user: %v", cust)
 	}
 }
