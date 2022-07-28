bfc2232f126cbf0bd821231fc3d90c75ef7cfcd8
Molly <molly.gouletas@gmail.com>
2022-07-27 17:38:57 -0700

    Add return type for GetTransactions to interface

--- adapters/handler_interfaces.go
+++ adapters/handler_interfaces.go
@@ -12,6 +12,6 @@ type IAuthProcessHandler interface {
 
 type ITransactionProcessHandler interface {
 	GetBalance(cust *entities.Customer) (float64, error)
-	GetTransactions(cust *entities.Customer, args ...map[string]interface{})
+	GetTransactions(cust *entities.Customer, args ...map[string]interface{}) ([]*entities.Transaction, error)
 	RecordTransaction(cust *entities.Customer, is_debit bool, amount float64, voided_tx int) (*entities.Transaction, error)
 }
